0c308030b1f55ba29b740ec96e012822a788d8bd
Julia <julia.v.wang@gmail.com>
2022-03-21 05:50:36 +0100

    Fucking fixade så att det går att använda knapparna se Description snälla
    Tog mig 3 timmar att lista ut att jag inte hade ett EventSystems objekt och jag fuckade upp också sen ARToPlace och fattade inte hur AR camera fungerade men det går nog att använda andra Render Modes för UI så det känns 3D

--- Assets/Scripts/AR/ARTapPlace.cs
+++ Assets/Scripts/AR/ARTapPlace.cs
@@ -18,7 +18,6 @@ public class ARTapPlace : MonoBehaviour
     [SerializeField]
     private ARSession session;
     
-    private GameObject placedObject;
     private Pose placementPose; // Simple data structure that represents a 3D-point
     private ARSessionOrigin arOrigin;
     private ARRaycastManager rayCastMgr; // Needed to Raycast
@@ -34,6 +33,7 @@ public class ARTapPlace : MonoBehaviour
         session = FindObjectOfType<ARSession>();
         rayCastMgr = this.GetComponent<ARRaycastManager>();
         arPlaneMgr = this.GetComponent<ARPlaneManager>();
+        objToPlace.SetActive(false);
     }
 
     // Update is called once per frame
@@ -51,15 +51,17 @@ public class ARTapPlace : MonoBehaviour
     public void PlaceObject() {
 
         placementPose.position.y += (float)0.5;
-        placedObject = Instantiate(objToPlace, placementPose.position, placementPose.rotation);
+        objToPlace.SetActive(true);
+        objToPlace.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         active = true;
         DisablePlanes();
     }
 
-    /* NOT TESTED YET STILL IMPLEMENTING
+    /*  ResetAR()
+        Removes the placed experiment and enables planes again.
     */
     public void ResetAR() {
-        Destroy(placedObject); //or objTopPlace.SetActive(false);
+        objToPlace.SetActive(false);
         active = false;
         EnablePlanes();
     }
